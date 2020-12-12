697661c4dd042fe72dde53bdb2fd3922735d74dc
RYUJAEEUN <fbwodms2004@naver.com>
2020-12-12 22:25:02 +0900

    실습예제

--- code/ch07/forlab.c
+++ code/ch07/forlab.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+ï»¿#include <stdio.h>
 #define MAX 9
 
 int main(void)
 {
-	printf("=== ±¸±¸´Ü Ãâ·Â ===\n");
+	printf("=== êµ¬êµ¬ë‹¨ ì¶œë ¥ ===\n");
 	for (int i = 2; i <= MAX; i++)
 	{
-		printf("%6d´Ü Ãâ·Â\n", i);
+		printf("%6dë‹¨ ì¶œë ¥\n", i);
 	}
 
 	return 0;
--- code/ch07/mtable.c
+++ code/ch07/mtable.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+ï»¿#include <stdio.h>
 #define MAX 9
 
 int main(void)
 {
-	printf("=== ±¸±¸´Ü Ãâ·Â ===\n");
+	printf("=== êµ¬êµ¬ë‹¨ ì¶œë ¥ ===\n");
 	for (int i = 2; i <= MAX; i++)
 	{
-		printf("%6d´Ü Ãâ·Â\n", i);
+		printf("%6dë‹¨ ì¶œë ¥\n", i);
 		for (int j = 2; j <= MAX; j++)
 			printf("%d*%d = %2d ", i, j, i * j);
 		printf("\n");
