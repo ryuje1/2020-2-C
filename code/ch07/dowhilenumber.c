fdc6d4e75d5ee0984e9bcf1a936f556fb0441441
RYUJAEEUN <fbwodms2004@naver.com>
2020-12-12 22:37:24 +0900

    Update dowhilenumber.c

--- code/ch07/dowhilenumber.c
+++ code/ch07/dowhilenumber.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+ï»¿#include <stdio.h>
 #define MAX 5
 
 int main(void)
@@ -10,7 +10,7 @@ int main(void)
 		printf("%d\n", n++);
 	} while (n <= MAX);
 
-	printf("\nÁ¦¾îº¯¼ö n => %d\n", n);
+	printf("\nì œì–´ë³€ìˆ˜ n => %d\n", n);
 
 	return 0;
 }
